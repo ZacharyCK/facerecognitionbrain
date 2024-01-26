7304014f4109c40f3e6177b86eac426d8e0c2bb2
Zachary Kohs <zachary.c.kohs@gmail.com>
2024-01-26 18:17:25 -0500

    integrated AI API to take photos and see how many human faces are detected

---
+++ src/components/facerecognition/FaceRecognition.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./FaceRecognition.css"
+
+const FaceRecognition = ({ imageURL, boxes }) => {
+    return (
+        <div className="center ma">
+            <div className="absolute mt2">
+                <img id="inputImage" src={imageURL} alt="input" width='500px' height='auto' />
+                {boxes.map((box, i) => (
+                    <div key={i} className="bounding-box" style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default FaceRecognition;
