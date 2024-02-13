fc04ce64a93607be2a48c5ee9220bfa85b3cfcb2
Zachary Kohs <zachary.c.kohs@gmail.com>
2024-02-13 13:45:59 -0500

    updated FaceRecognition component to only show the image element when there is an image

--- src/components/facerecognition/FaceRecognition.js
+++ src/components/facerecognition/FaceRecognition.js
@@ -5,7 +5,9 @@ const FaceRecognition = ({ imageURL, boxes }) => {
     return (
         <div className="center ma">
             <div className="absolute mt2">
-                <img id="inputImage" src={imageURL} alt="input" width='500px' height='auto' />
+                {
+                    imageURL && <img id="inputImage" src={imageURL} alt="input" width='500px' height='auto' />
+                }
                 {boxes.map((box, i) => (
                     <div key={i} className="bounding-box" style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
                 ))}
