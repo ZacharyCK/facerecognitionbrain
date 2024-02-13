6b133ca724b0b5d31af2dd0031c818e20725b15a
Zachary Kohs <zachary.c.kohs@gmail.com>
2024-02-13 15:14:11 -0500

    fixed typo in the Instructions

--- src/components/instructions/Instructions.js
+++ src/components/instructions/Instructions.js
@@ -7,7 +7,7 @@ const Instructions = () => {
         {'To try this app, get the URL of an image and place it in the input box above'}
       </p>
       <p className='f3'>
-        {'Search for an image on your favorite search engine and copy the addres(URL)'}
+        {'Search for an image on your favorite search engine and copy the address(URL)'}
       </p>
     </div>
   );
